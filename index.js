04f71eb1e5d47036bb29da0b5b0251385590c109
Gustavo Henrique Schmitz <redytdi@protonmail.com>
2022-01-17 22:14:51 -0300

    Java Script
    Adicionando a função de adicionar tarefas.

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-function add() {
 
+listajs = ['Estudar HTML', 'Estudar CSS', 'Estudar JS']
+
+const ol = document.querySelector("ol")
+
+function add() {
+    const input = document.querySelector("input")
+    listajs.push(input.value)
+    input.value = ""
+    render()
 }
 
 function render() {
-
+    ol.innerHTML = null
+    listajs.forEach(function (tar) {
+        const li = document.createElement("li")
+        li.innerText = tar
+        ol.appendChild(li)
+    });
 }
 
 function remove() {
 
 }
+
+render()
